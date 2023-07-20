fe15200809026367dbc5e3809d484ec6059de21d
Isaac Perkins <iperkins@uoregon.edu>
2023-07-20 15:34:26 -0700

    display plate information

--- src/PlateCollector.py
+++ src/PlateCollector.py
@@ -35,7 +35,12 @@ class PlateCollector:
             cv2.putText(frame, f"fps: {str(int(fps))}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
 
             for plate in plates:
-                frame = cv2.rectangle(frame, plate.top_left, plate.bottom_right, (255, 0, 0), 5)
+                text_size = cv2.getTextSize(str(plate), cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0]
+                text_x = int((plate.x1 + plate.x2 - text_size[0]) / 2)
+                text_y = int(plate.y1 - 10)
+
+                frame = cv2.rectangle(frame, plate.top_left, plate.bottom_right, (0, 0, 255), 5)
+                cv2.putText(frame, str(plate), (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
             cv2.imshow("Plate Detector", frame)
 
