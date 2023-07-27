ebdc44169aac746b0d596f90bc634647d5b62998
Isaac Perkins <iperkins@XPS-13.local>
2023-07-26 21:27:02 -0700

    Added command line argmuents

--- src/main.py
+++ src/main.py
@@ -1,38 +1,18 @@
 import os
+import argparse
 from PlateDetector import PlateDetector
 from PlateCollector import PlateCollector
-import cv2
-import numpy as np
 
 ROOT = os.path.abspath(".")
+PLATE_MODEL = os.path.join(ROOT, "models", "yolov5_nano.onnx")
 
-PATHS = {
-    "data": os.path.join(ROOT, "data"),
-    "models": os.path.join(ROOT, "models"),
-}
-
-DATA = {
-    "img00" : os.path.join(PATHS["data"], "car00.webp"),
-    "img01" : os.path.join(PATHS["data"], "car01.webp"),
-    "img02" : os.path.join(PATHS["data"], "car02.jpg"),
-    "img03" : os.path.join(PATHS["data"], "car03.jpg"),
-    "img04" : os.path.join(PATHS["data"], "car04.jpg"),
-    "video" : os.path.join(PATHS["data"], "video.mp4"),
-}
-
-MODELS = {
-    "plates_pt" : os.path.join(PATHS["models"], "plates.pt"),
-    "plates_n_pt" : os.path.join(PATHS["models"], "plates_n.pt"),
-    "plates_onnx" : os.path.join(PATHS["models"], "plates.onnx"),
-    "platesv8_pt" : os.path.join(PATHS["models"], "plates_yolov8.pt"),
-    "platesv8_onnx" : os.path.join(PATHS["models"], "plates_yolov8.onnx"),
-    "platesv5n_onnx" : os.path.join(PATHS["models"], "plates_yolov5n.onnx"),
-}
+parser = argparse.ArgumentParser(prog="PlateSpotter", description="License plate detection tool. Press ESC to exit the program.")
+parser.add_argument("-i", "--input", default=0, metavar="input_source", help="The input path of a video or 0 for camera capture (default: 0)")
 
 
 def main():
-    pd = PlateDetector(MODELS["platesv5n_onnx"], 640)
-    pc = PlateCollector(DATA["video"], pd)
+    pd = PlateDetector(PLATE_MODEL, 640)
+    pc = PlateCollector(parser.parse_args().input, "PlateSpotter", pd)
 
     pc.Run()
 
